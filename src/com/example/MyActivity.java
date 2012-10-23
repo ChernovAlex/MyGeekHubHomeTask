cc1b53a304a90dfffac28c3934ddc77f20bf3531
Alex <alex@R517.(none)>
2012-10-23 15:41:53 +0300

    test

--- src/com/example/MyActivity.java
+++ src/com/example/MyActivity.java
@@ -1,15 +1,33 @@
 package com.example;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.SearchView;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
-public class MyActivity extends Activity {
+public class MyActivity extends Activity{
     /**
      * Called when the activity is first created.
      */
     @Override
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        final Intent intent = new Intent(this,MySecondActivity.class);
+        final Button button = (Button) findViewById(R.id.f_btn);
+        button.setOnClickListener(new OnClickListener(){
+            public void onClick(View view){
+                //Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
---
+++ src/com/example/MySecondActivity.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alex
+ * Date: 23.10.12
+ * Time: 9:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class MySecondActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+    }
+}
